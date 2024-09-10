4b072f8455943bf392d160ad7c95f3d36cd0fae4
Haticha <108629307+Haticha@users.noreply.github.com>
2024-09-10 09:55:21 +0000

    initiation button functionality

---
+++ code.js
@@ -0,0 +1,44 @@
+const display = document.querySelector(`.display`)
+const inputs = document.querySelectorAll(`input`);
+for (let index = 0; index < inputs.length; index++) {
+    let inputGroup = inputs[index].className;
+    let inputValue = inputs[index].value;
+
+    inputGroup==`digits`?
+        inputs[index].addEventListener('click',digit(inputValue)):
+        inputs[index].addEventListener('click',taskExecute(inputGroup));
+
+    console.log(`event setup done ${inputGroup}:${inputValue}`);
+}
+function digit(num) {
+    console.log(num);
+}
+function taskExecute(task){
+    console.log(task);
+    switch (task) {
+        case `solution`:
+            
+        break;
+        case `clearLast`:
+
+        break;
+        case `clearAll`:
+
+        break;
+        case `backspace`:
+
+        break;
+        case `addition`:
+
+        break;
+        case `substraction`:
+
+        break;
+        case `multiplication`:
+
+        break;
+        case `devision`:
+
+        break;
+    }
+}
