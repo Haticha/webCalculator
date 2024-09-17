97c04ae3e439191ae91d6869575e0f85af2c1a07
Haticha <108629307+Haticha@users.noreply.github.com>
2024-09-17 09:18:42 +0000

    Number inputs and backspace functional

--- code.js
+++ code.js
@@ -7,29 +7,33 @@ for (let index = 0; index < inputs.length; index++) {
     let inputGroup = inputs[index].className;
     let inputValue = inputs[index].value;
     inputGroup==`digits`?
-        inputs[index].addEventListener('click',func => { digit(inputValue)}):
+        inputs[index].addEventListener('click',func => { constructNum(inputValue)}):
         inputs[index].addEventListener('click',func => {taskExecute(inputGroup)});
 }
 
 
-function digit(num) {
+function constructNum(num) {
     active? activeNum= `${activeNum}`+`${num}`: activeNum=num, active=true ;
-
+    console.log(activeNum , activeNum.length);
 }
 function taskExecute(task){
-    console.log(task);
     switch (task) {
         case `solution`:
             
         break;
         case `clearLast`:
-
+            activeNum = 0;
+            active = false;
         break;
         case `clearAll`:
-
+            //add other numbers of operation
+            activeNum = 0;
+            active = false;
         break;
         case `backspace`:
-
+            activeNum.length>1?
+            activeNum = activeNum.slice(0,-1):
+            activeNum = 0, active = false;
         break;
         case `addition`:
 
@@ -44,4 +48,5 @@ function taskExecute(task){
 
         break;
     }
+    console.log(activeNum , activeNum.length);
 }
