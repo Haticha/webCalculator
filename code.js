c2964482f19f18013a25a78130d50eb275428828
Haticha <108629307+Haticha@users.noreply.github.com>
2024-09-17 07:59:56 +0000

    number construction functional

--- code.js
+++ code.js
@@ -1,18 +1,20 @@
+let activeNum = 0
+let active = false
 const display = document.querySelector(`.display`);
 
 const inputs = document.querySelectorAll(`input`);
 for (let index = 0; index < inputs.length; index++) {
     let inputGroup = inputs[index].className;
     let inputValue = inputs[index].value;
-
     inputGroup==`digits`?
         inputs[index].addEventListener('click',func => { digit(inputValue)}):
         inputs[index].addEventListener('click',func => {taskExecute(inputGroup)});
-
-    console.log(`event setup done ${inputGroup}:${inputValue}`);
 }
+
+
 function digit(num) {
-    console.log(num);
+    active? activeNum= `${activeNum}`+`${num}`: activeNum=num, active=true ;
+
 }
 function taskExecute(task){
     console.log(task);
