cf58fff100ba78525e79b10613f1f623f8dfc269
Haticha <108629307+Haticha@users.noreply.github.com>
2024-09-17 10:25:53 +0000

    Initial task execution made

--- code.js
+++ code.js
@@ -1,5 +1,9 @@
-let activeNum = 0
-let active = false
+let activeNum = 0;
+let numInputActive = false;
+let primary = 0;
+let previousSolution = 0;
+let solutionOperator = null;
+let solution = 0;
 const display = document.querySelector(`.display`);
 
 const inputs = document.querySelectorAll(`input`);
@@ -11,42 +15,55 @@ for (let index = 0; index < inputs.length; index++) {
         inputs[index].addEventListener('click',func => {taskExecute(inputGroup)});
 }
 
-
 function constructNum(num) {
-    active? activeNum= `${activeNum}`+`${num}`: activeNum=num, active=true ;
+    numInputActive? activeNum= `${activeNum}`+`${num}`: activeNum=num, numInputActive=true ;
     console.log(activeNum , activeNum.length);
 }
 function taskExecute(task){
     switch (task) {
         case `solution`:
-            
+            secondary = activeNum;
+            solution = solutionOperator(primary,secondary);
         break;
         case `clearLast`:
             activeNum = 0;
-            active = false;
+            numInputActive = false;
         break;
         case `clearAll`:
             //add other numbers of operation
+            solutionOperator = null;
+            primary = 0;
             activeNum = 0;
-            active = false;
+            numInputActive = false;
         break;
         case `backspace`:
             activeNum.length>1?
             activeNum = activeNum.slice(0,-1):
-            activeNum = 0, active = false;
+            activeNum = 0, numInputActive = false;
         break;
         case `addition`:
-
+            numInputActive?primary = activeNum: primary = previousSolution;
+            numInputActive = false;
+            solutionOperator = func =>addition(primary,secondary)
         break;
         case `substraction`:
-
+            numInputActive?primary = activeNum: primary = previousSolution;
+            numInputActive = false;
+            solutionOperator = func =>substraction(primary,secondary)
         break;
         case `multiplication`:
-
+            numInputActive?primary = activeNum: primary = previousSolution;
+            numInputActive = false;
+            solutionOperator = func =>multiplication(primary,secondary)
         break;
         case `devision`:
-
+            numInputActive?primary = activeNum: primary = previousSolution;
+            numInputActive = false;
+            solutionOperator = func =>devision(primary,secondary)
         break;
     }
-    console.log(activeNum , activeNum.length);
 }
+function addition(primary,secondary) {return primary + secondary;}
+function substraction(primary,secondary) {return primary - secondary;}
+function multiplication(primary,secondary) {return primary * secondary;}
+function devision(primary,secondary) {return primary / secondary;}
